2b6ce0192ef686b8f212db6a67ac92cc17e0c76a
NiceTwice <onlynicetwice@gmail.com>
2022-11-12 23:06:45 -0300

    db url changed

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDB = () => {
-    mongoose.connect(process.env.MONGO_PASS, { useNewUrlParser: true })
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
         .then(data=>console.log("- Database Online -"))
         .catch(err=>console.log(err))
 } 
